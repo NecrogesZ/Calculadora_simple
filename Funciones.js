a7f039787ed4a10d2ab5147fda6b107b503dd649
NecrogesZ <129928675+NecrogesZ@users.noreply.github.com>
2025-06-02 19:54:53 -0400

    Calculadora

---
+++ Funciones.js
@@ -0,0 +1,41 @@
+function suma() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function resta() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function multiplica() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function divide() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 / num2;
+    document.getElementById("resultado").value = resultado;
+}
+function potencia() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 ** num2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function calcula() {
+    suma();
+    resta();
+    multiplica();
+    divide();
+    potencia();
+}
